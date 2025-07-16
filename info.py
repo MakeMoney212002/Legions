f4c1d9dd2c0c985a6981145f496e4e63db18046b
MakeMoney212002 <115358435+MakeMoney212002@users.noreply.github.com>
2025-07-16 14:43:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://makemoney212002:Ganesh@212002@legions.bujewss.mongodb.net/?retryWrites=true&w=majority&appName=Legions")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Dynasty:212002@dynasty.m47qtum.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Storage")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Storage')
 # Others
