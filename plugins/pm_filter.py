beb30994d15e9f0e1614efed9d726ecb346ff14a
MakeMoney212002 <115358435+MakeMoney212002@users.noreply.github.com>
2025-08-16 13:49:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -293,7 +293,7 @@ async def languages_cb_handler(client: Client, query: CallbackQuery):
 
   #  btn.insert(
   #      0,
-        [
+   #     [
      #       InlineKeyboardButton(
      #           text="👇 𝖲𝖾𝗅𝖾𝖼𝗍 𝖸𝗈𝗎𝗋 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 👇", callback_data="ident"
     #        )
